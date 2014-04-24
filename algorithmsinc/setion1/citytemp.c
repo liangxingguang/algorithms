b45f46e8536797d93cec1e0171d48dbd141f6793
liangxingguang <=>
2014-04-24 23:27:22 +0800

    complete the city's temp sort

--- algorithmsinc/setion1/citytemp.c
+++ algorithmsinc/setion1/citytemp.c
@@ -53,13 +53,13 @@ int AddNodeAscend( Link to_add )
 		i = NodeCmp( pn, curr );
 		if(i <= 0)
 		break;
-	}
+ 	}
 
 	if( curr && i == 0 )
 	{
 		if( DuplicateNode ( curr, pn) == 0 )
 			return ( 1 );
-	}
+ 	}
 
 	prev->Next = pn;
 	pn->Next = curr;
@@ -67,4 +67,145 @@ int AddNodeAscend( Link to_add )
 	return ( 0 );
 }
 
+int DuplicateNode ( Link inlist, Link duplicate )
+{
+	FreeNode( duplicate );
+	return ( 0 );
+}
 
+int DeleteNode ( Link to_delete )
+{
+	Link curr,
+		 prev;
+	int i;
+
+	if( Head == NULL )
+		return 0;
+
+	for ( prev = NULL, curr = Head;
+			curr != NULL && ( i = NodeCmp( to_delete, curr)) > 0;
+		  prev = curr, curr = curr->next )
+	{
+		if ( curr != NULL && i == 0)
+		{
+			if ( prev )
+				prev->Next = curr->Next;
+			else
+				Head = curr->Next;
+
+			FreeNode( curr );
+			NodeCount -= 1;
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
+int NodeCmp ( Link a, Link b )
+{
+
+	if(a->Temp != b->Temp )
+		return (a->Temp - b->Temp);
+	return strcmp( a->City, b->City);
+}
+
+void CreateList ( void )
+{
+	Head = NULL;
+	NodeCount = 0;
+}
+
+void FreeNode ( Link n )
+{
+	free( n->City);
+	free( n );
+}
+
+void ShowNodes( void )
+{
+	Link pn;
+	int count,median;
+
+	for ( count = 0, pn = Head; pn; pn = pn->Nex )
+	{
+		count += 1;
+	}
+
+	median = count / 2 + 1;
+
+	if( count )
+	{
+		count = 0;
+		for ( pn = Head; pn; pn = pn->Next )
+		{
+			printf( "%-20s: %3d", pn->City, pn->Temp );
+			count += 1;
+			if( count == median )
+				printf( " --Median--" );
+		}
+	}
+	else
+		printf( "Empty list\n" );
+
+}
+
+
+int main(int argc, char *argv[] )
+{
+	FILE *fin;
+	char buffer[128];
+
+	struct Node n;
+
+	if( argc != 2 )
+	{
+		fprintf( stderr, "Usage: citytemp filename.ext\n" );
+		exit(1);
+	}
+
+	fin = fopen(argv[1], "rt");
+	if(fin == NULL)
+	{
+		fprintf(stderr, "Cannot open/find %s\n", argv[2] );
+		exit(1);
+	}
+
+	CreateList();
+
+	while ( ! feof ( fine ) )
+	{
+		if( fgets( buffer, 127, fin) == NULL)
+			break;
+		buffer [ strlen( buffer ) - 1 ] = '\0';
+		n.Citye = strdup ( buffer + 3 );
+
+		buffer[3] = '\0';
+		n.Temp = atoi ( buffer );
+
+		if( AddNodeAscend ( &n ) == 0)
+		{
+			fprintf(stderr, "Error adding node. Aborting\n");
+			exit(1);
+		}
+	}
+
+	ShowNodes();
+	printf("\n");
+	DeleteNode( Head );
+	ShowNodes();
+
+	while (Head && Head->Next )
+	{
+		printf( "\n" );
+		DeleteNode ( Head->Next );
+		ShowNodes();
+	}
+
+	printf( "\n" );
+	DeleteNode( Head );
+	ShowNodes();
+
+	fclose( fin );
+	return ( 0 )
+}
